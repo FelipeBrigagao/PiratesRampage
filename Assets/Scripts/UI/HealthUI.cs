3b882dba0496f68e1bddc09ec387c0501d34f4c5
Felipe Brigagão <fgbrigs@gmail.com>
2023-11-27 17:57:11 -0300

    Bullet trail

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,5 +7,19 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
     [SerializeField] private Slider _healthSlider;
+
+    [SerializeField] private HealthBase _health;
+
+    private void Awake()
+    {
+        _healthSlider.maxValue = _health.MaxHealth;
+        _healthSlider.minValue = 0;
+    }
+
+    public void UpdateHealthBar()
+    {
+        _healthSlider.value = _health.CurrentHealth;
+    }
+    
     
 }
